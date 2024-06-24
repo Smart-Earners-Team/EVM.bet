9d994c2c9d60d54d1ac42fcdf5929ff8a3b483ff
EVMlord <85406816+EVMlord@users.noreply.github.com>
2024-06-24 10:22:41 -0700

    Add new etherlink contracts

--- src/hooks/addresses.ts
+++ src/hooks/addresses.ts
@@ -6,12 +6,14 @@ export const addresses = {
     lottery: <AddressType> {
         9768: "0xFbf681a9515A61be2cA3E75db83e15749A1907E9",
         // 128123: "0x2B6Aa77f23d7fC14161E540beFDfAE5e67eFb25d",
-        128123: "0xd820a73F78c47Ab06e7d6624e7d081ff35a2b9Ec"
+        // 128123: "0xd820a73F78c47Ab06e7d6624e7d081ff35a2b9Ec"
+        128123: "0x68B5E924DDDdA92dEA7F38f8dCB3386d4854c18F"
     },
     randomizer: <AddressType> {
         9768: "",
         // 128123: "0xCFB024DE4bD45700d53d91CDdF7836348DA3c8CC",
-        128123: "0x66053BDDA8FAB5E664858FD87B2e158258Cba3c2"        
+        // 128123: "0x66053BDDA8FAB5E664858FD87B2e158258Cba3c2" 
+        128123: "0x8b657bdbb185F7921d6bE08032Ac2ce2039a8481"         
     },
     
 }
